77f2824f8522051550258307bf86f9c7beb1899d
dasb <936336596@qq.com>
2025-03-26 16:59:24 +0800

    fix tags

--- src/main/java/com/yanny/age/stone/api/utils/Tags.java
+++ src/main/java/com/yanny/age/stone/api/utils/Tags.java
@@ -2,9 +2,9 @@ package com.yanny.age.stone.api.utils;
 
 import com.yanny.age.stone.Reference;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
-import net.minecraft.resources.ResourceLocation;
 
 public class Tags {
     public static class Blocks {
@@ -14,9 +14,9 @@ public class Tags {
     public static class Items {
         public static final TagKey<Item> AXES = tag(new ResourceLocation(Reference.MODID, "axes"));
         public static final TagKey<Item> HAMMERS = tag(new ResourceLocation(Reference.MODID, "hammers"));
-        public static final TagKey<Item> KNIVES = tag(new ResourceLocation("ages_api", "knives"));
-        public static final TagKey<Item> FISHING_NET_MESHES = tag(new ResourceLocation("ages_api", "fishing_net_meshes"));
-        public static final TagKey<Item> BONES = tag(new ResourceLocation("ages_api", "bones"));
+        public static final TagKey<Item> KNIVES = tag(new ResourceLocation("stone", "knives"));
+        public static final TagKey<Item> FISHING_NET_MESHES = tag(new ResourceLocation("stone", "fishing_net_meshes"));
+        public static final TagKey<Item> BONES = tag(new ResourceLocation("stone", "bones"));
 
         private static TagKey<Item> tag(ResourceLocation resourceLocation) {;
             return TagKey.create(Registries.ITEM, resourceLocation);
